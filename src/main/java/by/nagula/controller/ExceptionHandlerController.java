d74f4474a1b4fdb878b31c6702cf3fa55516bc51
KirillNagula <kiriliandia@mail.ru>
2020-12-07 22:34:32 +0300

    add all exception in handler. Change visu in html

--- src/main/java/by/nagula/controller/ExceptionHandlerController.java
+++ src/main/java/by/nagula/controller/ExceptionHandlerController.java
@@ -1,11 +1,13 @@
 package by.nagula.controller;
 
 import by.nagula.service.exception.EmptyDataFromControllerException;
+import by.nagula.service.exception.NoChatException;
 import by.nagula.service.exception.UserAlreadyExistException;
+import by.nagula.service.exception.WrongIdException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
 public class ExceptionHandlerController {
@@ -19,6 +21,48 @@ public class ExceptionHandlerController {
     @ExceptionHandler(EmptyDataFromControllerException.class)
     public String emptyData(Model model, EmptyDataFromControllerException ex){
         model.addAttribute("messageInReg2", ex.getMessage());
+        return "registration";
+    }
+
+    @ExceptionHandler(WrongIdException.class)
+    public String wrongId(Model model, WrongIdException ex){
+        model.addAttribute("wrongId", ex.getMessage());
+        return "chat";
+    }
+
+    @ExceptionHandler(WrongIdException.class)
+    public String wrongId2(Model model, WrongIdException ex){
+        model.addAttribute("wrongId", ex.getMessage());
+        return "task";
+    }
+
+    @ExceptionHandler(WrongIdException.class)
+    public String wrongId3(Model model, WrongIdException ex){
+        model.addAttribute("wrongId", ex.getMessage());
+        return "addUserInChat";
+    }
+
+    @ExceptionHandler(NoChatException.class)
+    public String wrongChat(Model model, NoChatException ex){
+        model.addAttribute("wrongChat", ex.getMessage());
+        return "chat";
+    }
+
+    @ExceptionHandler(UsernameNotFoundException.class)
+    public  String userNot(Model model, UsernameNotFoundException ex){
+        model.addAttribute("not found", ex.getMessage());
+        return "login";
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public String nullEr(Model model, NullPointerException ex){
+        model.addAttribute("null from controller", ex.getMessage());
+        return "account";
+    }
+
+    @ExceptionHandler(NullPointerException.class)
+    public String nullEr2(Model model, NullPointerException ex){
+        model.addAttribute("null from controller", ex.getMessage());
         return "registration";
     }
 }
